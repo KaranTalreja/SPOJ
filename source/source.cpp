eb9e015ef9f9cbc7f117ee96b24c26b3a1eee494
KaranTalreja <karantalreja91@gmail.com>
2014-01-14 02:03:46 +0530

    Added code for array generator

--- source/source.cpp
+++ source/source.cpp
@@ -9,15 +9,41 @@ int main()
 	for(int i=0;i<TestCases;i++)
 	{
 		cin>>N;
+		vector<int> first(N);
+		vector<int> temp(N);
+		vector<int> ::iterator start= first.begin();
 		int currentValue;
 		bool flag = true;
 		for (int j=0;j<N;j++)
 		{
 			cin>>currentValue;
-			if(currentValue > j)
-			{flag = false;
-			break;}
+			temp[j]=currentValue;
+			if(currentValue <= j)
+			{
+					first.insert(start + j - currentValue,j);
+					start = first.begin();
+			}
+			else
+			{
+				flag = false;
+				break;
+			}
+
+
 		}
+
+		vector<int> second(N);
+
+		for(int k=0;k<N;k++)
+			second[first[k]] = k+1;
+
+
+//		for(int k=0;k<N;k++)
+//			cout<<second[k]<<" ";
+//		cout<<endl;
+//		for(int k=0;k<N;k++)
+//				cout<<temp[k]<<" ";
+//			cout<<endl;
 		if(flag==false)
 			cout<<"NO"<<endl;
 		else
