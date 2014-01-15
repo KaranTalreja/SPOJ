3a8eb3edc34cbd80853c2498ba74e463c2ce84ee
Karan Talreja <karant@noida.atrenta.com>
2014-01-15 18:46:51 +0530

    Added final working code.
    Time:0.0
    Memory:1.6M

--- source/source.c
+++ source/source.c
@@ -6,25 +6,18 @@ int main()
 	long long NoOfChild;
 	int j;
 	int count=0;
-	long long parent;
 	scanf("%d",&TestCases);
 	for(j=0;j<TestCases;j++)
 	{
 		scanf("%d %llu",&Generation,&NoOfChild);
-		NoOfChild = ((0x00 | 0x01) << (Generation-1)) + NoOfChild -1;
-		//printf("temp->%llu \n no%llu\n",);
+		NoOfChild = NoOfChild -1;
 		count =0;
-		while(Generation != 1)
+		while(NoOfChild)
 		{
-			parent = NoOfChild/2;
-			count += (NoOfChild - (parent*2));
-			NoOfChild = parent;
-			Generation--;
+			count += (NoOfChild & 0x01);
+			NoOfChild = (NoOfChild >> 1);
 		}
 		((count & 0x01) == 1) ? printf("Female\n") : printf("Male\n");
-		/*for( i=0;i<N;i++)
-		{
-		}*/
 	}
 
 	return 0;
