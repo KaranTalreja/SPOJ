6831e9cc8773355d1915312d52ad8708de0fe100
KaranTalreja <karantalreja91@gmail.com>
2014-01-29 00:46:07 +0530

    Added Code for fast I/O using thread lock free version
    Run time 1.90sec to 0.19 sec on SPOJ

--- source/source.cpp
+++ source/source.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-using namespace std;
+#define getcx getchar_unlocked
+inline void inp( int &n )//fast input function
+{
+   n=0;
+   register int ch=getcx();int sign=1;
+   while( ch < '0' || ch > '9' ){if(ch=='-')sign=-1; ch=getcx();}
 
+   while(  ch >= '0' && ch <= '9' )
+           n = (n<<3)+(n<<1) + ch-'0', ch=getcx();
+   n=n*sign;
+}
+using namespace std;
+int array[100001] = {0};
 int main()
 {
 	int TestCases,At,Bt;
@@ -10,11 +21,11 @@ int main()
 	for (int i=0;i<TestCases;i++)
 	{
 		scanf("%d %d",&At,&Bt);
-		int *array = (int*)malloc(sizeof(int)*At);
+		//int *array = (int*)malloc(sizeof(int)*At);
 		pair<int,int> BestVal(0,0);
 		for(int j=0;j<At;j++)
 		{
-			scanf("%d",&array[j]);
+			inp(array[j]);
 		}
 		int k=0,sum=0;
 		for(int j=0;j<At;j++)
@@ -52,8 +63,8 @@ int main()
 				BestVal.second = k;
 			}
 		}
-		free(array);
-		array = NULL;
+		//free(array);
+		//array = NULL;
 		printf("%d %d\n",BestVal.first,BestVal.second);
 	}
 	return 0;
