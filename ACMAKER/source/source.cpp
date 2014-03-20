80b066d779ab30b0540f0eaeb522bf90e8607b6f
KaranTalreja <karantalreja91@gmail.com>
2014-03-21 01:11:14 +0530

    ACMAKER_dev: Added working algorithm for counting
    Added hard coded cases which work fine.
    Still to adapt to the site's input method

--- ACMAKER/source/source.cpp
+++ ACMAKER/source/source.cpp
@@ -13,14 +13,24 @@ inline void inpLine(char* str);
 int main()
 {
 	vector<char*> listOfWords;
-	listOfWords.push_back("THIS");
-	listOfWords.push_back("BOOO");
-	listOfWords.push_back("GOOD");
-	char* Acronym = "HOOD";
+//	listOfWords.push_back("THHS");
+//	listOfWords.push_back("BOOO");
+//	listOfWords.push_back("GOOD");
+//	char* Acronym = "HOOD";
+//	listOfWords.push_back("academy");
+//	listOfWords.push_back("computer");
+//	listOfWords.push_back("makers");
+//	char* Acronym = "acm";
+	listOfWords.push_back("apple");
+	listOfWords.push_back("day");
+	char* Acronym = "apply";
+//	listOfWords.push_back("radio");
+//	listOfWords.push_back("detection");
+//	listOfWords.push_back("ranging");
+//	char* Acronym = "radar";
 	size_t lenOfAcr = strlen(Acronym);
 	vector<vector<vector<int> > > Cache;
-	vector<vector<vector<int> > > Count2;
-	vector<vector<int> > Count;
+	vector<vector<vector<int> > > Count;
 	for(size_t i=0;i<=lenOfAcr;i++)
 	{
 		vector<vector<int> >tmp;
@@ -32,15 +42,13 @@ int main()
 			tmp.push_back(tmpVec);
 		}
 		Cache.push_back(tmp);
-		Count2.push_back(tmp);
-		Count.push_back(vector<int>(listOfWords.size()+1));
+		Count.push_back(tmp);
 	}
 
 	for(size_t i=1;i<Cache.size();i++)
 	{
 		for(size_t j=1;j<Cache[i].size();j++)
 		{
-			int multiply = 0;
 			for(size_t k=1;k<Cache[i][j].size();k++)
 			{
 
@@ -49,47 +57,30 @@ int main()
 					if(Acronym[i-1] == listOfWords[j-1][k-1])
 					{
 						Cache[i][j][k] = 1;
-						//Count[i][j] = 1;
-						multiply++;
-						Count2[i][j][k] = multiply;
+						Count[i][j][k] = 1 + Count[i-1][j][k-1] + Count[i-1][j-1].back() + Count[i][j][k-1];
 					}
 					else
 					{
 						Cache[i][j][k] = Cache[i][j][k-1];
-						Count2[i][j][k] = Count2[i][j][k-1];
+						Count[i][j][k] = Count[i][j][k-1];
 					}
 				}
 				else
 				{
 					if(Acronym[i-1] == listOfWords[j-1][k-1])
 					{
-						multiply++;
 						Cache[i][j][k] = Cache[i-1][j][k-1] || Cache[i-1][j-1].back();
-						Count2[i][j][k] += Count2[i-1][j][k-1] + Count2[i-1][j-1].back()*multiply ;
-						//multiply= multiply + Cache[i-1][j][k-1] + Cache[i-1][j-1].back();
+						Count[i][j][k] = Count[i-1][j][k-1] + Count[i-1][j-1].back() + Count[i][j][k-1];
 					}
 					else
 					{
 						Cache[i][j][k] = Cache[i][j][k-1];
-						Count2[i][j][k] = Count2[i][j][k-1];
+						Count[i][j][k] = Count[i][j][k-1];
 					}
 				}
 			}
-			//Count[i][j] = Count[i-1][j] == 0 ? multiply : Count[i-1][j] * multiply;
-			//Count[i][j] = Count[i-1][j-1];
 		}
 	}
-
-//		for(size_t i=0;i<Count.size();i++)
-//		{
-//			for(size_t j=0;j<Count[i].size();j++)
-//			{
-//
-//					cout<<Count2[i][j]<<" ";
-//			}
-//			cout<<endl;
-//		}
-
 
 	for(size_t i=0;i<Cache.size();i++)
 	{
@@ -99,14 +90,14 @@ int main()
 			for(size_t k=0;k<Cache[i][j].size();k++)
 			{
 
-				cout<<Count2[i][j][k]<<" ";
+				cout<<Count[i][j][k]<<" ";
 			}
 			cout<<endl;
 		}
 		cout<<endl;
 	}
 
-	cout<<"!! "<<Cache.back().back().back()<<"!! !! "<<Count2.back().back().back()<<endl;
+	cout<<"!! "<<Cache.back().back().back()<<"!! !! "<<Count.back().back().back()<<endl;
 	return 0;
 }
 inline int getInt(char **str)
