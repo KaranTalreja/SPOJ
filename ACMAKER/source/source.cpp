be8b78de60a48646bf67dcf2459cd8c3557817cd
KaranTalreja <karantalreja91@gmail.com>
2014-03-22 02:04:54 +0530

    ACMAKER_dev: Added final accepted version
    Also added testcases

--- ACMAKER/source/source.cpp
+++ ACMAKER/source/source.cpp
@@ -2,102 +2,131 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <tr1/unordered_map>
+#include <sstream>
+//#include <tr1/unordered_set>
+#include <set>
 #include <string.h>
 using namespace std;
 #define getcx getchar_unlocked
 inline void inp( int &n );//fast input function
 inline int getInt(char **str);
 inline void inpLine(char* str);
-
+struct ltstr {
+    bool operator() (const char* lhs, const char* rhs) const{
+        return strcmp(lhs,rhs) < 0;
+    }
+};
 int main()
 {
 	vector<char*> listOfWords;
-//	listOfWords.push_back("THHS");
-//	listOfWords.push_back("BOOO");
-//	listOfWords.push_back("GOOD");
-//	char* Acronym = "HOOD";
-//	listOfWords.push_back("academy");
-//	listOfWords.push_back("computer");
-//	listOfWords.push_back("makers");
-//	char* Acronym = "acm";
-	listOfWords.push_back("apple");
-	listOfWords.push_back("day");
-	char* Acronym = "apply";
-//	listOfWords.push_back("radio");
-//	listOfWords.push_back("detection");
-//	listOfWords.push_back("ranging");
-//	char* Acronym = "radar";
-	size_t lenOfAcr = strlen(Acronym);
 	vector<vector<vector<int> > > Cache;
 	vector<vector<vector<int> > > Count;
-	for(size_t i=0;i<=lenOfAcr;i++)
+	char* Acronym;
+	set<string> insignificant;
+	int noOfInSignificantWords = 0;
+	while (1)
 	{
-		vector<vector<int> >tmp;
-		tmp.push_back(vector<int> (1));
-		for(size_t j=0;j<listOfWords.size();j++)
+		char* temp = (char*)malloc(150*sizeof(char));
+		char* back = temp;
+		inp(noOfInSignificantWords);
+		if(0 == noOfInSignificantWords)
+			break;
+		insignificant.clear();
+		for(int i=0;i<noOfInSignificantWords;i++)
 		{
-			size_t tmpLen = strlen(listOfWords[j]);
-			vector<int> tmpVec(tmpLen+1);
-			tmp.push_back(tmpVec);
+			inpLine(temp);
+			insignificant.insert(temp);
 		}
-		Cache.push_back(tmp);
-		Count.push_back(tmp);
-	}
-
-	for(size_t i=1;i<Cache.size();i++)
-	{
-		for(size_t j=1;j<Cache[i].size();j++)
+		while(1)
 		{
-			for(size_t k=1;k<Cache[i][j].size();k++)
-			{
+			temp = back;
+			inpLine(temp);
+			if(0 == strcmp(temp,"LAST CASE"))
+				break;
 
-				if(i == 1 && j == 1)
-				{
-					if(Acronym[i-1] == listOfWords[j-1][k-1])
-					{
-						Cache[i][j][k] = 1;
-						Count[i][j][k] = 1 + Count[i-1][j][k-1] + Count[i-1][j-1].back() + Count[i][j][k-1];
-					}
-					else
-					{
-						Cache[i][j][k] = Cache[i][j][k-1];
-						Count[i][j][k] = Count[i][j][k-1];
-					}
-				}
+			char* pos = strchr(temp,' ');
+			*pos = '\0';
+			listOfWords.clear();
+			Acronym = temp;
+			temp = pos + 1;
+			bool toBreak =false;
+			while(1)
+			{
+				pos = strchr(temp,' ');
+				if(pos)
+					*pos = '\0';
 				else
+					toBreak = true;
+				char* word = temp;
+				temp = pos + 1;
+				if(insignificant.end() == insignificant.find(word))
+					listOfWords.push_back(word);
+				if(toBreak)
+					break;
+			}
+			size_t lenOfAcr = strlen(Acronym);
+			for(size_t i=0;i<=lenOfAcr;i++)
+			{
+				vector<vector<int> >tmp;
+				tmp.push_back(vector<int> (1));
+				for(size_t j=0;j<listOfWords.size();j++)
 				{
-					if(Acronym[i-1] == listOfWords[j-1][k-1])
+					size_t tmpLen = strlen(listOfWords[j]);
+					vector<int> tmpVec(tmpLen+1);
+					tmp.push_back(tmpVec);
+				}
+				Cache.push_back(tmp);
+				Count.push_back(tmp);
+			}
+
+			for(size_t i=1;i<Cache.size();i++)
+			{
+				for(size_t j=1;j<Cache[i].size();j++)
+				{
+					for(size_t k=1;k<Cache[i][j].size();k++)
 					{
-						Cache[i][j][k] = Cache[i-1][j][k-1] || Cache[i-1][j-1].back();
-						Count[i][j][k] = Count[i-1][j][k-1] + Count[i-1][j-1].back() + Count[i][j][k-1];
-					}
-					else
-					{
-						Cache[i][j][k] = Cache[i][j][k-1];
-						Count[i][j][k] = Count[i][j][k-1];
+
+						if(i == 1 && j == 1)
+						{
+							if(Acronym[i-1] + 32 == listOfWords[j-1][k-1])
+							{
+								Cache[i][j][k] = 1;
+								Count[i][j][k] = 1 + Count[i-1][j][k-1] + Count[i-1][j-1].back() + Count[i][j][k-1];
+							}
+							else
+							{
+								Cache[i][j][k] = Cache[i][j][k-1];
+								Count[i][j][k] = Count[i][j][k-1];
+							}
+						}
+						else
+						{
+							if(Acronym[i-1] + 32 == listOfWords[j-1][k-1])
+							{
+								Cache[i][j][k] = Cache[i-1][j][k-1] || Cache[i-1][j-1].back();
+								Count[i][j][k] = Count[i-1][j][k-1] + Count[i-1][j-1].back() + Count[i][j][k-1];
+							}
+							else
+							{
+								Cache[i][j][k] = Cache[i][j][k-1];
+								Count[i][j][k] = Count[i][j][k-1];
+							}
+						}
 					}
 				}
 			}
-		}
-	}
-
-	for(size_t i=0;i<Cache.size();i++)
-	{
-		cout<<"I == "<<i<<endl;
-		for(size_t j=0;j<Cache[i].size();j++)
-		{
-			for(size_t k=0;k<Cache[i][j].size();k++)
+			if(Cache.back().back().back())
 			{
-
-				cout<<Count[i][j][k]<<" ";
+				printf("%s can be formed in %d ways\n",Acronym,Count.back().back().back());
 			}
-			cout<<endl;
+			else
+			{
+				printf("%s is not a valid abbreviation\n",Acronym);
+			}
+			Cache.clear();
+			Count.clear();
 		}
-		cout<<endl;
 	}
-
-	cout<<"!! "<<Cache.back().back().back()<<"!! !! "<<Count.back().back().back()<<endl;
 	return 0;
 }
 inline int getInt(char **str)
@@ -125,7 +154,7 @@ inline void inpLine(char *str)
 	register int i = 0;
 	while (c < 33)
 		c = getcx();
-	while (c != '\n' && c != ' ') {
+	while (c != '\n'/* && c != ' '*/) {
 		str[i] = c;
 		c = getcx();
 		i = i + 1;
